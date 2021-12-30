d0be533bd5dbfd5ffc0da48033af7234801e9559
Edward Arechwa <e.arechwa@gmail.com>
2021-12-30 21:26:34 +0100

    add connection to mongo

---
+++ src/configs/mongo.ts
@@ -0,0 +1,19 @@
+import { ConfigService } from '@nestjs/config';
+import { TypegooseModuleOptions } from 'nestjs-typegoose';
+
+export const getMongoConfig = async (configService: ConfigService): Promise<TypegooseModuleOptions> => {
+  return { uri: getMongoUri(configService), ...getMongoOptions() };
+};
+
+const getMongoUri = (configService: ConfigService): string =>
+  'mongodb://' +
+  configService.get('MONGO_LOGIN') +
+  configService.get('MONGO_PASSWORD') +
+  '@' +
+  configService.get('MONGO_HOST') +
+  ':' +
+  configService.get('MONGO_PORT') +
+  '/' +
+  configService.get('MONGO_AUTH_DATABASE');
+
+const getMongoOptions = () => ({});
