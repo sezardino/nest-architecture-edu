12fbd004d99d2b3b0fd3152c3efbee0acfdc91dc
Edward Arechwa <e.arechwa@gmail.com>
2021-12-30 21:26:46 +0100

    integrate models with typegoose

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypegooseModule } from 'nestjs-typegoose';
 import { ReviewController } from './review.controller';
 
 @Module({
-  controllers: [ReviewController]
+  controllers: [ReviewController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: ReviewModule,
+        schemaOptions: { collection: 'Product' }
+      }
+    ])
+  ]
 })
 export class ReviewModule {}
--- src/top-page/top-page.module.ts
+++ src/top-page/top-page.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypegooseModule } from 'nestjs-typegoose';
 import { TopPageController } from './top-page.controller';
 
 @Module({
-  controllers: [TopPageController]
+  controllers: [TopPageController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: TopPageModule,
+        schemaOptions: { collection: 'Product' }
+      }
+    ])
+  ]
 })
 export class TopPageModule {}
